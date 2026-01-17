92c01ce3d60323959eafe3962f3179fe27f4a637
Eddie Mooney <brnagn7@protonmail.com>
2026-01-17 11:40:18 -0500

    Initial Commit

---
+++ Property Manager/Property_ManagerApp.swift
@@ -0,0 +1,32 @@
+//
+//  Property_ManagerApp.swift
+//  Property Manager
+//
+//  Created by Edward Mooney on 1/17/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Property_ManagerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
